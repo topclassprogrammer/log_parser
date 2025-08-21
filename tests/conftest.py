fdb7cc6cd9e925da2e35abfb8268412f058da791
topclassprogrammer <topclassprogrammer@gmail.com>
2025-08-21 12:49:37 +0300

    added fixture column_name()

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,3 +7,8 @@ def args() -> argparse.Namespace:
     argparser = argparse.Namespace(
         file=["example1.log"], report="average", date="2025-06-22")
     return argparser
+
+
+@pytest.fixture
+def column_name() -> str:
+    return "url"
