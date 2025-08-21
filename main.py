5f92423c10021ee77173559489ba262a58afbf13
topclassprogrammer <topclassprogrammer@gmail.com>
2025-08-21 12:42:05 +0300

    added func get_column_count()

--- main.py
+++ main.py
@@ -25,3 +25,21 @@ def get_dict_lines_list(args: argparse.Namespace) -> list[dict]:
             print(f"File {filename.strip(".\\")} not found " 
                   f"in the current directory.")
     return dict_lines_list
+
+
+def get_column_count(column_name: str, dict_lines_list: list[dict]) \
+        -> list[dict] | None:
+    if not column_name:
+        return None
+    dict_ = {}
+    for x in dict_lines_list:
+        try:
+            if x[column_name] in dict_:
+                dict_[x[column_name]] += 1
+            else:
+                dict_[x[column_name]] = 1
+        except KeyError as e:
+            print(f"Column {e} not found")
+            return None
+    list_ = [{column_name: k, "total": v} for k, v in dict_.items()]
+    return list_
