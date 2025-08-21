8c28366cf29886153d9c20821544c7afb558f03f
topclassprogrammer <topclassprogrammer@gmail.com>
2025-08-21 12:41:28 +0300

    added func get_dict_lines_list()

--- main.py
+++ main.py
@@ -11,3 +11,17 @@ def get_parsed_args() -> argparse.Namespace:
     parser.add_argument("--date")
     args = parser.parse_args()
     return args
+
+
+def get_dict_lines_list(args: argparse.Namespace) -> list[dict]:
+    dict_lines_list = []
+    for filename in args.file:
+        try:
+            with open(filename, "r", encoding="utf-8") as f:
+                for line in f.readlines():
+                    dict_line = json.loads(line)
+                    dict_lines_list.append(dict_line)
+        except FileNotFoundError:
+            print(f"File {filename.strip(".\\")} not found " 
+                  f"in the current directory.")
+    return dict_lines_list
