f9abda8e9905dd70aab377e739c44d2ab8d65f84
topclassprogrammer <topclassprogrammer@gmail.com>
2025-08-21 12:49:03 +0300

    added fixture args()

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+import argparse
+import pytest
+
+
+@pytest.fixture
+def args() -> argparse.Namespace:
+    argparser = argparse.Namespace(
+        file=["example1.log"], report="average", date="2025-06-22")
+    return argparser
